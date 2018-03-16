09e06f437c365c932c591b3ccebce34d16e49373
Arnaud Bienner <abien@dolby.com>
2018-03-16 23:16:34 +0100

    Actually implement the processing in C. Pass data to the C function, which expects float*

--- audio-worklet/basic/js/wasm/bypass.c
+++ audio-worklet/basic/js/wasm/bypass.c
@@ -4,7 +4,6 @@
 // input and output should have the same size, defined in size
 void process_bypass(float* input, float* output, int size)
 {
-  printf("process_bypass\n");
   for (int i = 0; i < size; ++i) {
     output[i] = input[i];
   }
--- audio-worklet/basic/js/wasm/post.js
+++ audio-worklet/basic/js/wasm/post.js
@@ -14,14 +14,33 @@ class BypassProcessor extends AudioWorkletProcessor {
   }
 
   process(inputs, outputs) {
-    let input = inputs[0];
-    let output = outputs[0];
-    for (let channel = 0; channel < output.length; ++channel) {
-      output[channel].set(input[channel]);
-    }
+    // Process first channel only i.e. supports only mono for now
+    let input = inputs[0][0];
+    let output = outputs[0][0];
 
-    //Module.process();
-    Module._process_bypass();
+    // TODO: passing array to C is painful: factorize the code below as much as
+    // possible
+    // Get data byte size, allocate memory on Emscripten heap, and get pointer
+    var nDataBytesInput = input.length * input.BYTES_PER_ELEMENT;
+    var inputPtr = Module._malloc(nDataBytesInput);
+
+    // Copy data to Emscripten heap (directly accessed from Module.HEAPU8)
+    var inputHeap = new Uint8Array(Module.HEAPU8.buffer, inputPtr, nDataBytesInput);
+    inputHeap.set(new Uint8Array(input.buffer));
+
+    // Get data byte size, allocate memory on Emscripten heap, and get pointer
+    var nDataBytesOuput = output.length * output.BYTES_PER_ELEMENT;
+    var outputPtr = Module._malloc(nDataBytesOuput);
+
+    // Copy data to Emscripten heap (directly accessed from Module.HEAPU8)
+    var outputHeap = new Uint8Array(Module.HEAPU8.buffer, outputPtr, nDataBytesOuput);
+    outputHeap.set(new Uint8Array(output.buffer));
+
+    // Call function and get result
+    Module.ccall('process_bypass', 'number', ['number', 'number', 'number'], [inputHeap.byteOffset, outputHeap.byteOffset, input.length]);
+    var result_input = new Float32Array(inputHeap.buffer, inputHeap.byteOffset, input.length);
+    var result_output = new Float32Array(outputHeap.buffer, outputHeap.byteOffset, output.length);
+    output.set(result_output);
 
     return true;
   }
